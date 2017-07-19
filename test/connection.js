231a2e188e0841b5029ea42a675fd84c248e7ba9
Bingmang <ustbbingmang@foxmail.com>
2017-07-19 18:01:59 +0800

    TG-001 [Fixed] UnhandledRejection in define

--- test/connection.js
+++ test/connection.js
@@ -1,37 +1,37 @@
-describe('connection', function () {
+describe('connection', function() {
 
-  it('action restriction', function (done) {
-    const client = sqlx.createClient()
-    client.define('table1', {
-      delete: function (table, where, callback) { n_called++; callback() },
-      update: function (table, sets, where, callback) { n_called++; callback() },
-      select: function (table, fields, where0, callback) { n_called++; callback() },
-    }).catch((err) => {
-      assert(!err)
-    })
-
-
-    var n_called = 0
-    var operator_info = {
-      user: '101,23',
-      actions: ['delete'],
-    }
-
-    const conn = client.getConnection(operator_info)
-    assert.equal(n_called, 0)
-    conn.update('table1', {}, {}, function (err) {
-      assert(err)
-      assert(err.toString().match(/action .* not allowed/))
-    })
-    conn.delete('table1', {}, function (err) {
-      assert(!err)
-    })
-    setTimeout(function () {
-      assert.equal(n_called, 1)
-      conn.release()
-      done()
-    }, 50)
+it('action restriction', function(done) {
+  const client = sqlx.createClient()
+  client.define('table1'  , {
+    delete: function (table, where , callback)         { n_called++; callback() },
+    update: function (table, sets  , where, callback)  { n_called++; callback() },
+    select: function (table, fields, where0, callback) { n_called++; callback() },
+  }).catch((err) => {
+    assert(!err)
   })
+
+
+  var n_called = 0
+  var operator_info = {
+    user: '101,23',
+    actions: ['delete'],
+  }
+
+  const conn = client.getConnection(operator_info)
+  assert.equal(n_called, 0)
+  conn.update('table1', {}, {}, function(err){
+    assert(err)
+    assert(err.toString().match(/action .* not allowed/))
+  })
+  conn.delete('table1', {}, function(err){
+    assert(!err)
+  })
+  setTimeout(function() {
+    assert.equal(n_called, 1)
+    conn.release()
+    done()
+  }, 50)
+})
 
 
 })
